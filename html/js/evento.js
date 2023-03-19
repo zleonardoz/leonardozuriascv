30dc5ed62be77f997c569036f230f0bc42792f88
Leonardo Zurias <zuriasleonardo@gmail.com>
2023-03-19 11:04:24 -0300

    actualizacion del proyecto + actualizacion de la calculadora

--- html/js/evento.js
+++ html/js/evento.js
@@ -2,3 +2,13 @@ function redireccion(){
     location.href = "formulario.html"
 }
 
+function redireccion2(){
+    location.href = "calculadora.html"
+}
+
+var boton = document.getElementById('numero1');
+ var resultado = document.getElementById('numeroa');
+
+    boton.onclick = function(e){
+      numeroa.value +=  this.value;
+ }
